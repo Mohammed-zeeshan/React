ee46a6b31756394f4539df6afcf063f0202af8b1
Mohammed-zeeshan <117900168+Mohammed-zeeshan@users.noreply.github.com>
2023-11-23 02:26:41 +0530

    Cart open and close

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
-import { Fragment } from 'react';
-import Header from './components/Layout/Header';
+import Modal from '../UI/Modal'
+import classes from './Cart.module.css'
 
-function App() {
+const Cart = (props) => {
+    const cartItems = (
+        <ul className={classes['cart-item']}>
+            {[{id: 'c1', name: 'Sushi', amount: 2,price: 12.99}].map((item) => (
+                <p>{item.name}</p>
+            ))}
+        </ul>
+    )
   return (
-    <Fragment>
-      <Header />
-    </Fragment>
-  );
+    <Modal>
+        {cartItems}
+        <div className={classes.total}>
+            <span>Total Amount</span>
+            <span>35.62</span>
+        </div>
+        <div className={classes.actions}>
+            <button className={classes['button--alt']} onClick={props.onHideCart}>Close</button>
+            <button className={classes.button}>Order</button>
+        </div>
+    </Modal>
+  )
 }
 
-export default App;
+export default Cart
