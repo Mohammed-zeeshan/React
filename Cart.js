7f05b0a3ca7f23be344681a5f5a55712036ca720
Mohammed-zeeshan <117900168+Mohammed-zeeshan@users.noreply.github.com>
2023-11-24 03:03:46 +0530

    DIY - Showing and Enabling the delete of Cart Items

--- Cart.js
+++ Cart.js
@@ -1,23 +1,34 @@
+import { useContext } from 'react'
 import Modal from '../UI/Modal'
 import classes from './Cart.module.css'
+import CartContext from '../../store/cart-context'
 
-const Cart = () => {
+
+const Cart = (props) => {
+    const cartctx = useContext(CartContext)
+    let total = 0;
     const cartItems = (
         <ul className={classes['cart-item']}>
-            {[{id: 'c1', name: 'Sushi', amount: 2,price: 12.99}].map((item) => (
-                <p>{item.name}</p>
+            {cartctx.items.map((item) => (
+                <div key={item.id}>
+                    <li>Name: {item.name} Price: {item.price} Quantity: {item.quantity} </li>
+                    <button className={classes['button--alt']} onClick={()=>item.quantity = item.quantity-1}>-</button><button className={classes.button}>+</button>
+                </div>
             ))}
         </ul>
     )
+    cartctx.items.forEach(item => {
+        total = total + item.price;
+    })
   return (
-    <Modal>
+    <Modal onHideCart={props.onHideCart}>
         {cartItems}
         <div className={classes.total}>
             <span>Total Amount</span>
-            <span>35.62</span>
+            <span>{total}</span>
         </div>
         <div className={classes.actions}>
-            <button className={classes['button--alt']}>Close</button>
+            <button className={classes['button--alt']} onClick={props.onHideCart}>Close</button>
             <button className={classes.button}>Order</button>
         </div>
     </Modal>
